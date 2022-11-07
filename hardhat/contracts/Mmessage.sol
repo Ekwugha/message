ebf02bed8dd0c023428e851de8ff20ff2ecd0865
Ekwugha <elloekwugha@gmail.com>
2022-11-07 11:29:35 +0100

    updated frontend

--- hardhat/contracts/Mmessage.sol
+++ hardhat/contracts/Mmessage.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.4;
 contract Message {
     mapping(address => string) public myMessage;
 
-    function setMessage(address _addr, string memory _message) public {
-        myMessage[_addr] = _message;
+    function setMessage(string memory _message) public {
+        myMessage[msg.sender] = _message;
     }
 
-    function getMessage(address _addr) public view returns (string memory) {
-        return myMessage[_addr];
+    function getMessage(address _address) public view returns (string memory) {
+        return myMessage[_address];
     }
 }
--- my-appp/constants/index.js
+++ my-appp/constants/index.js
@@ -1,59 +1,54 @@
-export const MESSAGE_CONTRACT_ADDRESS = "0x52345158ef041452E3FD5d0154af7BFE23e34914";
+export const MESSAGE_CONTRACT_ADDRESS = "0xcE6d03970Cc965d5418cb82732609bFC1420597b";
 export const abi = [
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "_addr",
-          "type": "address"
-        }
-      ],
-      "name": "getMessage",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "name": "myMessage",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "_addr",
-          "type": "address"
-        },
-        {
-          "internalType": "string",
-          "name": "_message",
-          "type": "string"
-        }
-      ],
-      "name": "setMessage",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }
-  ];
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_address",
+        "type": "address"
+      }
+    ],
+    "name": "getMessage",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "myMessage",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_message",
+        "type": "string"
+      }
+    ],
+    "name": "setMessage",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
